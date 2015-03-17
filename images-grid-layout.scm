f2cd771a2bd4a5e1ecb991ee692e0d61a70d55f8
Kamil Svoboda <kamil.svoboda@centrum.cz>
2015-03-17 20:51:18 +0100

    Version upgrade

--- images-grid-layout.scm
+++ images-grid-layout.scm
@@ -1,4 +1,4 @@
-; IGLO Images Grid LayOut  1.6
+; IGLO Images Grid LayOut  1.6.1
 ; Easy way to print multiple photos on single paper
 ; 
 ; Copyright (C) 2005-2015 Kamil Svoboda <email: kamil (dot) svoboda (at sign) centrum (dot) cz
